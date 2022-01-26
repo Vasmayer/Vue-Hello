9545615354bf42c111d08946f2fd9673d73ea3cb
Vasmayer <lealimarco90@gmail.com>
2022-01-26 15:57:07 +0100

    added image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
 console.log('Vue JS OK',Vue);
 
 Vue.config.devtools = true;
+
+var app5 = new Vue({
+    el: '#root',
+    data: {
+      image: 'https://picsum.photos/200'
+    }
+    
+  })
